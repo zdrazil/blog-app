0bb845d6bb534b035b6a95ff01808f690a25906e
Vladimír Zdražil <mail@vzdrazil.com>
2018-09-22 21:11:58 +0200

    Remove navigation, add dumb homepage and styles

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,26 +1,16 @@
-module Main exposing (Model, Msg(..), init, main, subscriptions, update, view, viewLink)
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
-import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Url
 
 
 
 -- MAIN
 
 
-main : Program () Model Msg
 main =
-    Browser.application
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        , onUrlChange = UrlChanged
-        , onUrlRequest = LinkClicked
-        }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -28,14 +18,13 @@ main =
 
 
 type alias Model =
-    { key : Nav.Key
-    , url : Url.Url
+    { name : String
     }
 
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    ( Model key url, Cmd.none )
+init : Model
+init =
+    Model "hello"
 
 
 
@@ -43,25 +32,14 @@ init flags url key =
 
 
 type Msg
-    = LinkClicked Browser.UrlRequest
-    | UrlChanged Url.Url
+    = NoOp
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        LinkClicked urlRequest ->
-            case urlRequest of
-                Browser.Internal url ->
-                    ( model, Nav.pushUrl model.key (Url.toString url) )
-
-                Browser.External href ->
-                    ( model, Nav.load href )
-
-        UrlChanged url ->
-            ( { model | url = url }
-            , Cmd.none
-            )
+        NoOp ->
+            model
 
 
 
@@ -77,23 +55,121 @@ subscriptions _ =
 -- VIEW
 
 
-view : Model -> Browser.Document Msg
+view : Model -> Html Msg
 view model =
-    { title = "URL Interceptor"
-    , body =
-        [ text "The current URL is: "
-        , b [] [ text (Url.toString model.url) ]
-        , ul []
-            [ viewLink "/home"
-            , viewLink "/profile"
-            , viewLink "/reviews/the-century-of-the-self"
-            , viewLink "/reviews/public-opinion"
-            , viewLink "/reviews/shah-of-shahs"
+    div [ class "home-page" ]
+        [ div [ class "banner" ]
+            [ div [ class "container" ]
+                [ h1 [ class "logo-font" ]
+                    [ text "conduit" ]
+                , p []
+                    [ text "A place to share your knowledge." ]
+                ]
+            ]
+        , div [ class "container page" ]
+            [ div [ class "row" ]
+                [ div [ class "col-md-9" ]
+                    [ div [ class "feed-toggle" ]
+                        [ ul [ class "nav nav-pills outline-active" ]
+                            [ li [ class "nav-item" ]
+                                [ a [ class "nav-link disabled", href "" ]
+                                    [ text "Your Feed" ]
+                                ]
+                            , li [ class "nav-item" ]
+                                [ a [ class "nav-link active", href "" ]
+                                    [ text "Global Feed" ]
+                                ]
+                            ]
+                        ]
+                    , div [ class "article-preview" ]
+                        [ div [ class "article-meta" ]
+                            [ a [ href "profile.html" ]
+                                [ img [ src "http://i.imgur.com/Qr71crq.jpg" ]
+                                    []
+                                ]
+                            , div [ class "info" ]
+                                [ a [ class "author", href "" ]
+                                    [ text "Eric Simons" ]
+                                , span [ class "date" ]
+                                    [ text "January 20th" ]
+                                ]
+                            , button [ class "btn btn-outline-primary btn-sm pull-xs-right" ]
+                                [ i [ class "ion-heart" ]
+                                    []
+                                , text "29            "
+                                ]
+                            ]
+                        , a [ class "preview-link", href "" ]
+                            [ h1 []
+                                [ text "How to build webapps that scale" ]
+                            , p []
+                                [ text "This is the description for the post." ]
+                            , span []
+                                [ text "Read more..." ]
+                            ]
+                        ]
+                    , div [ class "article-preview" ]
+                        [ div [ class "article-meta" ]
+                            [ a [ href "profile.html" ]
+                                [ img [ src "http://i.imgur.com/N4VcUeJ.jpg" ]
+                                    []
+                                ]
+                            , div [ class "info" ]
+                                [ a [ class "author", href "" ]
+                                    [ text "Albert Pai" ]
+                                , span [ class "date" ]
+                                    [ text "January 20th" ]
+                                ]
+                            , button [ class "btn btn-outline-primary btn-sm pull-xs-right" ]
+                                [ i [ class "ion-heart" ]
+                                    []
+                                , text "32            "
+                                ]
+                            ]
+                        , a [ class "preview-link", href "" ]
+                            [ h1 []
+                                [ text "The song you won't ever stop singing. No matter how hard you try." ]
+                            , p []
+                                [ text "This is the description for the post." ]
+                            , span []
+                                [ text "Read more..." ]
+                            ]
+                        ]
+                    ]
+                , div [ class "col-md-3" ]
+                    [ div [ class "sidebar" ]
+                        [ p []
+                            [ text "Popular Tags" ]
+                        , div [ class "tag-list" ]
+                            [ a [ class "tag-pill tag-default", href "" ]
+                                [ text "programming" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "javascript" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "emberjs" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "angularjs" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "react" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "mean" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "node" ]
+                            , a [ class "tag-pill tag-default", href "" ]
+                                [ text "rails" ]
+                            ]
+                        ]
+                    ]
+                ]
             ]
         ]
-    }
+        |> withStyle
 
 
-viewLink : String -> Html Msg
-viewLink path =
-    li [] [ a [ href path ] [ text path ] ]
+withStyle html =
+    div []
+        [ node "style"
+            [ type_ "text/css" ]
+            [ text "@import url(https://demo.productionready.io/main.css)" ]
+        , html
+        ]
